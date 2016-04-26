87f07103c613129e848965cb53a40fe81da61e95
Erik Öhrn <erik.ohrn@outlook.com>
2016-04-26 15:43:57 +0200

    Markov code added

---
+++ markov/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char x[5000000];
+
+int main()
+{int c, i, eqsofar, max, n = 0, k = 3;
+ char *p, *nextp, *q;
+ while ((c = getchar()) != EOF)
+   x[n++] = c;
+ x[n] = 0;
+ p = x;
+ srand(1);
+ for (max = 2000; max > 0; max--) {
+   eqsofar = 0;
+   for (q = x; q < x + n - k + 1; q++) {
+     for (i = 0; i < k && *(p+i) == *(q+i); i++)
+       ;
+     if (i == k)
+       if (rand() % ++eqsofar == 0)
+	 nextp = q;
+   }
+   c = *(nextp+k);
+   if (c == 0)
+     break;
+   putchar(c);
+   p = nextp+1;
+ }
+ return 0;
+}
